c9b3a2cc3c278b94ede79b7778b73abb2c2b3bb4
Rosa <82625333+ayahana19@users.noreply.github.com>
2021-04-18 02:27:06 +0900

    Add files via upload

---
+++ hello html/hello.js
@@ -0,0 +1,14 @@
+﻿// JavaScript source code
+var app_1 = new Vue({
+	el: '#app_1',
+	data: {
+		// オブジェクトデータ
+		message_1: {
+			value: 'Hello Vue.js! (*´ω｀*)'
+		},
+		// 配列データ
+		list: ['りんご', 'ばなな', 'いちご'],
+		// 別のデータを使用してlistから取り出す要素を動的に
+		num: 1
+	}
+})
